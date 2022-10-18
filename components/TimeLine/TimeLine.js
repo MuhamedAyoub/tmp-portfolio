cc6ce4fb84527b1665f051bb5610bd130800ed36
MoAyoub <medayb45esi@gmail.com>
2022-10-18 12:40:07 +0100

    end time_line

--- components/TimeLine/TimeLine.js
+++ components/TimeLine/TimeLine.js
@@ -25,9 +25,9 @@ const Timeline = () => {
 	const [activeItem, setActiveItem] = useState(0);
 	const carouselRef = useRef();
 
-	// const scroll = (node, left) => {
-	//   return node.scrollTo({ left, behavior: 'smooth' });
-	// }
+	const scroll = (node, left) => {
+		return node.scrollTo({ left, behavior: 'smooth' });
+	};
 
 	const ClickHandler = (e, i) => {
 		e.preventDefault();
@@ -41,32 +41,34 @@ const Timeline = () => {
 		}
 	};
 
-	// const handleScroll = () => {
-	//   if (carouselRef.current) {
-	//     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+	const ScrollHandler = () => {
+		if (carouselRef.current) {
+			const index = Math.round(
+				(carouselRef.current.scrollLeft /
+					(carouselRef.current.scrollWidth * 0.7)) *
+					TimeLineData.length
+			);
 
-	//     setActiveItem(index);
-	//   }
-	// }
+			setActiveItem(index);
+		}
+	};
 
-	// // snap back to beginning of scroll when window is resized
-	// // avoids a bug where content is covered up if coming from smaller screen
-	// useEffect(() => {
-	//   const handleResize = () => {
-	//     scroll(carouselRef.current, 0);
-	//   }
+	useEffect(() => {
+		const handleResize = () => {
+			scroll(carouselRef.current, 0);
+		};
 
-	//   window.addEventListener('resize', handleResize);
-	// }, []);
+		addEventListener('scroll', handleResize);
+	}, []);
 
 	return (
-		<Section>
+		<Section about="about">
 			<SectionTitle>About Me </SectionTitle>
 			<SectionText>
 				the purpose of this section is to provide information about the
 				applications
 			</SectionText>
-			<CarouselContainer ref={courseRef}>
+			<CarouselContainer onScroll={ScrollHandler} ref={carouselRef}>
 				<>
 					{TimeLineData.map((item, index) => (
 						<CarouselMobileScrollNode
@@ -78,7 +80,7 @@ const Timeline = () => {
 								active={activeItem}
 								onClick={(e) => ClickHandler(e, index)}>
 								<CarouselItemTitle>
-									{item.year}
+									{`${item.year}`}
 									<CarouselItemImg
 										width="208"
 										height="6"
@@ -89,14 +91,46 @@ const Timeline = () => {
 											fill-rule="evenodd"
 											clip-rule="evenodd"
 											d="M2.5 5.5C3.8871 5.5.5 4.38871 5 3V3"
-											fill="url(#paint_linear)"></path>
+											fill="url(#paint_linear)"
+											fillOpacity="0.33"
+										/>
+										<defs>
+											<linearGradient
+												id="pain0_linear"
+												x1="-4.30412e-10"
+												y1="0.5"
+												x2="200"
+												y2="0.500295"
+												gradientUnits="userSpaceOneUSer">
+												<stop stopColor="white" />
+												<stop
+													offset="0.79478"
+													stop-color="white"
+													stop-opacity="0"
+												/>
+											</linearGradient>
+										</defs>
 									</CarouselItemImg>
 								</CarouselItemTitle>
+								<CarouselItemText>{item.text}</CarouselItemText>
 							</CarouselItem>
 						</CarouselMobileScrollNode>
 					))}
 				</>
 			</CarouselContainer>
+			<CarouselButtons>
+				{TimeLineData.map((item, index) => (
+					<CarouselButton
+						key={index}
+						index={index}
+						onClick={(e) => ClickHandler(e, index)}
+						active={activeItem}
+						type="button">
+						<CarouselButtonDot active={activeItem} />
+					</CarouselButton>
+				))}
+			</CarouselButtons>
+			<SectionDivider />
 		</Section>
 	);
 };
